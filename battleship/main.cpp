84e53d4381698ce6143a7f0861993b083debfb88
Lorissagg93 <lorissagg93@gmail.com>
2019-10-03 15:35:17 +0200

    Attempt to implement ship placement and shoot loop

--- battleship/main.cpp
+++ battleship/main.cpp
@@ -26,31 +26,69 @@ int main()
     Board boardPlayer1;
     Board boardPlayer2;
     //boardPlayer1.print();
-    boardPlayer1.insertShip(0, 1, 0, 3);
-    boardPlayer1.print();
-
-    cout << endl;
-
-    boardPlayer2.insertShip(1, 4, 5, 4);
-    boardPlayer2.print();
+    for(int t = 1; t <= 2; t++)
+    {
+        for(int p = 0; p < 5; p++)
+        {
+            if(t == 1)
+            {
+                cout << "Player 1 ";
+            }
+            else
+            {
+                cout << "Player 2 ";
+            }
+            // cout migliorabile
+            cout << "enter coordinate for ship placement" << endl;
+            cout << "Enter first x coordinate: ";
+            int x = 0 ;
+            cin >> x;
+            cout << "Enter first y coordinate: ";
+            int y = 0 ;
+            cin >> y;
+            cout << "Enter second x coordinate: ";
+            int x2 = 0 ;
+            cin >> x2;
+            cout << "Enter second y coordinate: ";
+            int y2 = 0 ;
+            cin >> y2;
+            if(t == 1)
+            {
+                boardPlayer1.insertShip(x, y, x2, y2);
+                boardPlayer1.print();
+                cout << endl;
+            }
+            else
+            {
+                boardPlayer2.insertShip(x, y, x2, y2);
+                boardPlayer2.print();
+                cout << endl;
+            }
+            
+        }
+    }
 
     // 
-    boardPlayer2.shoot(2, 4);
-    boardPlayer2.shoot(2, 5);
-    boardPlayer2.checkIfHit();
+    while(true)
+    {
+        cout << "Player1 enter shoot coordinate: ";
+        // modify to take imput coordinate for shoot
+        boardPlayer2.shoot(2, 4);
+        boardPlayer2.checkIfHit();
 
-    boardPlayer2.print();
+        boardPlayer2.print();
 
-    boardPlayer2.shoot(7, 8);
-    boardPlayer2.checkIfHit();
-    boardPlayer2.print();
+        boardPlayer2.shoot(7, 8);
+        boardPlayer2.checkIfHit();
+        boardPlayer2.print();
+    }
 
     // TODO:
-    // Loop di inserimento delle navi per i due giocatori
-    // for (0 5) cin int; insertShip
+    // (done) Loop di inserimento delle navi per i due giocatori (riga 29)
+    // (done) for (0 5) cin int; insertShip (riga 31)(fix need for ship over another ship placement)
     // Potenziare l'espressività del print() board
     // label string argomento
-    // Alternanza player per sparare
+    // (done) Alternanza player per sparare (riga 74)
 
     //
 }
